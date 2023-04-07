9fa4da66137eebdea21403b46a8af0c699219e45
reeman9000 <preston.soper@student.awesomeinc.org>
2023-04-07 14:01:04 +0000

    .

--- app/src/App.js
+++ app/src/App.js
@@ -5,7 +5,7 @@ const BASE_URL = 'https://8000-psoper1-djangojamsproje-mukak3eo3sv.ws-us93.gitpo
 
 function App() {
     const [genres, setGenres] = useState([])
-    const inputRef = useRef('');
+    // const inputRef = useRef('');
 
     useEffect(() => {
         const getGenres = async () => {
@@ -20,25 +20,18 @@ function App() {
 
         getGenres();
     }, [])
-    
-    async function getSongs() {
-        let config = {
-            url: '/albums-list/',
-            baseURL: BASE_URL,
-            method: 'get',
-        }
-        let response = await axios.request(config);
-        setGenres(response.data)
-        return (
-            <h1>{response.data.name}</h1>
-        )
-    }
+
+
+    // const handleClick = () => {
+    //     axios.get(BASE_URL)
+    //     .then(res => {setGenres(res.data)})
+    // }
     
     
     return (
         <>
-        <input type='text' ref={inputRef} />
-        <button onClick={getSongs}>Get Songs</button>
+        {/* <input type='text' ref={inputRef} />
+        <button onClick={handleClick}>Get Songs</button> */}
         {genres.map((genre) => 
         <div>
             <h1>Artist: {genre?.artist?.name}</h1>
